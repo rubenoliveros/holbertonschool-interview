220a0cb41997ea85db742b5c7c8b29bffd711873
Ruben Oliveros <ruben.oliveros@gmail.com>
2022-01-06 22:27:01 -0500

    Creating files

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""0. Minimum Operations"""
+
+
+def minOperations(n):
+    """A function that calculates the accuracy of a prediction"""
+    if not isinstance(n, int):
+        return 0
+    if n <= 0 or n == 1 or n is None:
+        return 0
+    if n % 2 == 0 and n != 2:
+        return minOperations(int(n / 2)) + 2
+    elif n % 3 == 0 and n != 3:
+        return minOperations(int(n / 3)) + 3
+    elif n % 5 == 0 and n != 5:
+        return minOperations(int(n / 5)) + 5
+    else:
+        return int(n)
