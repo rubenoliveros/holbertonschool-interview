c5408e15f79d50eff1aea123585456b0cee8784a
Ruben Oliveros <ruben.oliveros@gmail.com>
2022-01-06 22:40:23 -0500

    Tried another approach

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -4,15 +4,11 @@
 
 def minOperations(n):
     """A function that calculates the accuracy of a prediction"""
-    if not isinstance(n, int):
-        return 0
-    if n <= 0 or n == 1 or n is None:
-        return 0
-    if n % 2 == 0 and n != 2:
-        return minOperations(int(n / 2)) + 2
-    if n % 3 == 0 and n != 3:
-        return minOperations(int(n / 3)) + 3
-    f n % 5 == 0 and n != 5:
-        return minOperations(int(n / 5)) + 5
-    else:
-        return int(n)
+    t = 0
+    m = 2
+    while n > 1:
+        while not n % m:
+            t += m
+            n /= m
+        m += 1
+    return t
