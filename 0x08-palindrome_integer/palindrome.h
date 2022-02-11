dfff7e52693e8f7e9cc88172281bad2ebb2aaa06
Ruben Oliveros <ruben.oliveros@gmail.com>
2022-02-10 21:01:19 -0500

    First version

---
+++ 0x08-palindrome_integer/palindrome.h
@@ -0,0 +1,12 @@
+#ifndef _PALINDROME_H_
+#define _PALINDROME_H_
+
+/* Standard libraries */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* Palindrome prototype function */
+int is_palindrome(unsigned long n);
+
+#endif /* _PALINDROME_H_ */
