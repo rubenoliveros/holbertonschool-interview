ff827028d817776c555e48b53ea79f5412b011d9
Ruben Oliveros <ruben.oliveros@gmail.com>
2022-03-24 22:11:31 -0500

    Creating files

---
+++ 0x0D-sorted_array_to_avl/binary_trees.h
@@ -0,0 +1,31 @@
+#ifndef AVL_SORT_TREE_H
+#define AVL_SORT_TREE_H
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+};
+
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s avl_t;
+
+avl_t *sorted_array_to_avl(int *array, size_t size);
+avl_t *solution(int *array, int start, int end, avl_t *parent);
+void binary_tree_print(const binary_tree_t *);
+
+#endif /* AVL_SORT_TREE_H */
