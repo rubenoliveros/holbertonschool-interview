23904f59880bd02293fc51906b41324a689a1b2c
Ruben Oliveros <ruben.oliveros@gmail.com>
2022-03-24 22:18:14 -0500

    Creating files

--- 0x0D-sorted_array_to_avl/binary_trees.h
+++ 0x0D-sorted_array_to_avl/binary_trees.h
@@ -1,10 +1,14 @@
-#ifndef AVL_SORT_TREE_H
-#define AVL_SORT_TREE_H
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
 
+/* INCLUDED LIBRARIES */
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+
+/* STRUCTS AND DEFINITIONS */
+/* Basic Binary Tree */
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -13,19 +17,26 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-struct binary_tree_s
+typedef struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
-};
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+} binary_tree_t;
 
-typedef struct binary_tree_s binary_tree_t;
+/* AVL Tree */
 typedef struct binary_tree_s avl_t;
 
-avl_t *sorted_array_to_avl(int *array, size_t size);
-avl_t *solution(int *array, int start, int end, avl_t *parent);
+/* FUNCTION PROTOTYPES */
+/* function to print binary tree with formatting */
 void binary_tree_print(const binary_tree_t *);
 
-#endif /* AVL_SORT_TREE_H */
+/* function to build AVL tree from sorted array */
+avl_t *sorted_array_to_avl(int *array, size_t size);
+/* function to add nodes to AVL tree from sorted array recursively */
+int add_node_avl(int *array, size_t size, avl_t **tree, int add_left);
+/* function to free AVL tree if malloc fails creating a later node */
+void free_avl(avl_t **tree);
+
+#endif /* _BINARY_TREES_H_ */
