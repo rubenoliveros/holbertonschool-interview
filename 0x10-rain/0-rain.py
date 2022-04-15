4eb0291e9a7a81d50bab5b3ecdad45ff57fb627c
Ruben Oliveros <ruben.oliveros@gmail.com>
2022-04-14 21:16:06 -0500

    Second version

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -7,7 +7,8 @@ def rain(walls):
     Prototype: def rain(walls)
     walls is a list of non-negative integers.
     Return: Integer indicating total amount of rainwater retained.
-    Assume that the ends of the list (before index 0 and after index walls[-1]) are not walls, meaning they will not retain water.
+    Assume that the ends of the list (before index 0 and after index walls[-1])
+    are not walls, meaning they will not retain water.
     If the list is empty return 0.
     """
 
