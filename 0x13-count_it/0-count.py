62ba2328aaae70e1f4959a84559516f9bb2143fa
Ruben Oliveros <ruben.oliveros@gmail.com>
2022-11-20 02:11:29 -0500

    Fixing permissions

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -2,60 +2,58 @@
 """
 0x13. Count it!
 """
+import collections
 import requests
 
 
-def recurse(subreddit, word_list, hot_titles, after=""):
-    """Recursive function that queries the Reddit API and
-    prints a sorted count of given keywords
+def fill_list(subreddit, hot_list=[], after=None):
+    """Create list of words in hot titles of specified subreddit
     Args:
-        subreddit: string representing subreddit to search for
-        word_list: collection of keywords to search in the
-        subreddit
-        hot_titles: dictionary of number of occurrences of elements
-        in word_list that are found in the request
-        after: pagination feature of the reddit api
-    Return:
-        hot_titles or None
+        subreddit: string containing subreddit to search
+        hot_list: list to populate
+        after: pagination for next entry
+    Returns: populated list or None of failure
     """
-    url = "https://www.reddit.com/r/{}/hot.json?after={}".format(subreddit,
-                                                                 after)
-    res = requests.get(url,
-                       headers={'User-agent': 'product'},
+    req = requests.get("https://www.reddit.com/r/{}/hot.json?after={}".
+                       format(subreddit, after),
+                       headers={"User-agent": "agent"},
                        allow_redirects=False)
-
-    if res.status_code != 200:
+    if req.status_code != 200:
         return None
+    after = req.json().get("data").get("after")
     if after is None:
-        return hot_titles
-
-    for i in res.json().get('data').get('children'):
-        title_s = i.get('data').get('title').split()
-        for word in set(word_list):
-            if word.lower() in [x.lower() for x in title_s]:
-                if hot_titles.get(word):
-                    hot_titles[word] += 1
-                else:
-                    hot_titles[word] = 1
-
-    after = res.json().get('data').get('after')
-    recurse(subreddit, word_list, hot_titles, after)
-    return hot_titles
+        return hot_list
+    for i in req.json().get("data").get("children"):
+        for word in i.get("data").get("title").split():
+            hot_list.append(word.lower())
+    return fill_list(subreddit, hot_list, after)
 
 
 def count_words(subreddit, word_list):
-    """Calls recurse to get dictionary of ocurrences of word_list
-    and prints that dictionary sorted by values in decreasing order
+    """Finds occurences of specified keywords in a given subreddit
+    Prints keyword along with their occurrences in descending order. Keywords
+    with no matches are skipped
     Args:
-        subreddit: string representing subreddit to search for
-        word_list: collection of keywords to search in the
-        subreddit
-    Return:
-        each keyword with its corresponding count in order
+        subreddit: string containing subreddit to search
+        word_list: list of keywords to search for
+    Returns: OrderedDict with keys as keywords and occurrences as values or
+    None on request failure
     """
-    hot_titles = recurse(subreddit, word_list, {})
-    if hot_titles:
-        for k, v in sorted(hot_titles.items(), key=lambda val: val[1],
-                           reverse=True):
-            if v != 0:
-                print('{}: {}'.format(k, v))
+    if subreddit is None or subreddit == "" or word_list is None:
+        return None
+    hot_list = fill_list(subreddit)
+    if hot_list is None:
+        return None
+    all_cnt = collections.Counter(hot_list)
+    filtered_cnt = {}
+    for word in word_list:
+        word_l = word.lower()
+        if all_cnt[word_l] > 0:
+            if word in filtered_cnt:
+                filtered_cnt[word] += filtered_cnt[word]
+            else:
+                filtered_cnt[word] = all_cnt[word_l]
+    for k, v in sorted(filtered_cnt.items(),
+                       key=lambda item: item[1], reverse=True):
+        print("{}: {}".format(k, v))
+    return filtered_cnt
