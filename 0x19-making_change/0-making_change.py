b3431b04ed399d1f9ac616eaaab56cf56bce72b2
Ruben Oliveros <ruben.oliveros@gmail.com>
2022-11-20 19:10:01 -0500

    Fixing permissions

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -1,22 +1,32 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
 """
-makeChange method
+0x19. Making Change
 """
 
 
 def makeChange(coins, total):
     """
-    Given a pile of coins of different values, determine the fewest number
-    of coins needed to meet a given amount
+    Return: fewest number of coins needed to meet `total`.
+        * If total is 0 or less, return 0.
+        * If total cannot be met by any number of coins you have, return -1.
+    - Your solution’s runtime will be evaluated in this task.
     """
     if total <= 0:
         return 0
 
-    coins.sort(reverse=True)
-    count = 0
-    for coin in coins:
-        count += total // coin
-        total = total % coin
-    if total:
+    length = len(coins)
+
+    optimized = [0 for _ in range(total + 1)]
+
+    for i in range(1, total + 1):
+        smallest = float("inf")
+        for j in range(length):
+            if (coins[j] <= i):
+                smallest = min(smallest, optimized[i - coins[j]])
+        optimized[i] = 1 + smallest
+
+    if type(optimized[total]) is not int:
         return -1
-    return count
+
+    return optimized[total]
